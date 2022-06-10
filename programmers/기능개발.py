f9c5596463a97e0673dc54e981d1ff40f6cdba1e
dev-bong <qhddnjswo011@gmail.com>
2022-06-10 10:41:03 +0900

    :bulb: "기능개발" 문제 코멘트 추가

--- programmers/기능개발.py
+++ programmers/기능개발.py
@@ -9,7 +9,7 @@ class Queue():
         dq_items = [self.queue[0]]
         dq_size = 1
         for item in self.queue[1:]:
-            if dq_items[0] >= item:
+            if dq_items[0] >= item: # 앞의 일보다 먼저 또는 같이 끝나는 일들은 한번에 dequeue
                 dq_items.append(item)
                 dq_size += 1
             else:
@@ -25,7 +25,7 @@ def solution(progs, speeds):
     remain_days = Queue()
     res = []
 
-    for i in range(len(progs)):
+    for i in range(len(progs)): # 남은 일수 계산해서 리스트 만들기
         remain_prog = 100 - progs[i]
         remain_day = remain_prog // speeds[i] if remain_prog % speeds[i] == 0 else remain_prog // speeds[i] + 1
         remain_days.enqueue(remain_day)
