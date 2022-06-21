eb4f727b66c1dec19e771c9ec38bdeac8e5f3a8b
dev-bong <qhddnjswo011@gmail.com>
2022-06-21 16:48:49 +0900

    :sparkles: "전화번호 목록" 문제 풀이 추가
    hash 이용한 풀이 추가

--- programmers/correct/전화번호_목록.py
+++ programmers/correct/전화번호_목록.py
@@ -7,4 +7,64 @@ def solution(phone_book):
             return False
 
     return True
-# TODO : hash로 풀어보기!
+
+##### hash로 풀기 #####
+class Hash():
+    """
+    hash 구조
+    e.g. "347" 넣으면 아래처럼 됨
+    hash_table = {
+        "3" : {
+            "is_end" : False,
+            "4" : {
+                "is_end" : False,
+                "7" : {
+                    "is_end" : True
+                }
+            }
+        }
+    }
+
+    table 완성 시키고 숫자넣어서 tree search 처럼 찾아가서 도달한 곳에 "is_end"를 보고 접두사인지 판별
+    "is_end"가 False면 접두사임
+    ? 그냥 tree로 만들어 볼까??
+    """
+    def __init__(self):
+        self.hash_table = {}
+
+    def put_data(self, num): # 테이블에 데이터 넣기
+        prefix = False
+        tmp = self.hash_table
+        size = len(num)
+        for n in range(size):
+            if num[n] in tmp:
+                if n == (size - 1):
+                    if not tmp[num[n]]["is_end"]: # 숫자 끝에 도달했을때 "is_end" = False 이면 접두사
+                        prefix = True
+                else:
+                    tmp[num[n]]["is_end"] = False
+            else:
+                tmp[num[n]] = {}
+                if n == (size - 1):
+                    tmp[num[n]]["is_end"] = True
+                else:
+                    tmp[num[n]]["is_end"] = False
+            tmp = tmp[num[n]]
+            
+        return prefix
+
+def solution2(phone_book):
+    pb_hash = Hash()
+    phone_book.sort(reverse = True) # 사전식 역순으로 정렬해서 접두사 체크 용이하도록..
+    for num in phone_book:
+        if pb_hash.put_data(num): # 테이블 완성하지 않아도 중간에 접두사 발견하면 리턴
+            return False
+
+    return True
+
+"""
+원래는 테이블 다 완성시킨 후 하나씩 넣어보면서 접두사 체크하는 거였는데 (phone_book 정렬 X)
+마지막 테스트 케이스에서 시간 초과가 나서 phone_book 정렬하고, 데이터 넣어보면서 접두사 체크 동시에 하는걸로 변경
+? 정렬할거면 그냥 1번 풀이가 나은거 같은데..
+? tree로 한번 해보기
+"""
