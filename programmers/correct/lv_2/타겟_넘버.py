ae5766f313f4eb23132d011f9cd690e13c136323
dev-bong <qhddnjswo011@gmail.com>
2022-07-15 16:53:21 +0900

    ⚡️ "타겟 넘버" 문제 풀이 추가
    BFS, DFS 이용한 풀이 추가

--- programmers/correct/lv_2/타겟_넘버.py
+++ programmers/correct/lv_2/타겟_넘버.py
@@ -52,4 +52,54 @@ def make_tree(node, remains): # 트리 만들기
         return
 """
 
-# TODO : dfs, bfs로 간단히 풀어보기?? (근데 위 풀이도 dfs?느낌아닌가)
+# bfs, dfs 풀이 결과 위 풀이보다 성능 up
+##### bfs 이용한 풀이 #####
+
+from collections import deque
+"""
+그냥 리스트로 queue를 사용하게 되면 
+    queue.pop(0) 이런 식으로 사용하게 되는데
+    파이썬 list의 경우 stack에 가까워서?? pop(0)는 O(N)임
+반면에 deque의 queue.popleft()는 O(1)
+
+그래서 리스트로 했을땐 테스트케이스 1,2가 시간초과였는데
+deque 쓰니까 가볍게 통과
+
+어차피 재방문할 일이 없어서? visited 없어도 됨
+"""
+
+def solution(numbers, target):
+    answer = 0
+    queue = deque([(0, -1)]) # 옆에 -1은 numbers에서 index를 의미 (0보다 전)
+    len_num = len(numbers)
+
+    while queue:
+        num, cur_idx = queue.popleft()
+        if cur_idx == len_num - 1: # 리프노드? 중에
+            if num == target: # 조건에 맞는 답 찾으면 답 개수 +1
+                answer += 1
+            continue
+        next_num1, next_num2 = (num + numbers[cur_idx+1], num - numbers[cur_idx+1])
+        queue.append((next_num1, cur_idx+1))
+        queue.append((next_num2, cur_idx+1))
+
+    return answer
+
+##### dfs 이용한 풀이 #####
+
+def solution(numbers, target):
+    answer = 0
+    stack = [(0, -1)]
+    len_num = len(numbers)
+
+    while stack:
+        num, cur_idx = stack.pop()
+        if cur_idx == len_num - 1:
+            if num == target:
+                answer += 1
+            continue
+        next_num1, next_num2 = (num + numbers[cur_idx+1], num - numbers[cur_idx+1])
+        stack.append((next_num1, cur_idx+1))
+        stack.append((next_num2, cur_idx+1))
+
+    return answer
